aa9f5c83babbb17df67916dcce4ac8385f1966b0
gemithomas <42350582+gemithomas@users.noreply.github.com>
2019-08-07 22:56:29 -0400

    Correction to precision

--- NeuralNetwork/src/main/java/NeuralProjects/DigitRecognition.java
+++ NeuralNetwork/src/main/java/NeuralProjects/DigitRecognition.java
@@ -13,14 +13,14 @@ public class DigitRecognition {
 
 	public static void main(String[] args) throws IOException {
 		
-
+		long start_pgm = System.currentTimeMillis();
         //create hidden layer that has 4 neurons and 3 inputs per neuron
         NNLayer layer1 = new NNLayer(20, 784);
 
         // create output layer that has 1 neuron representing the prediction and 4 inputs for this neuron
         // (mapped from the previous hidden layer)
-        NNLayer layer2 = new NNLayer(10, 20);
-        NNLayer layer3 = new NNLayer(10, 10);
+        NNLayer layer2 = new NNLayer(20, 20);
+        NNLayer layer3 = new NNLayer(10, 20);
         
         //NNLayer layer4 = new NNLayer(10, 10);
         ArrayList<NNLayer> list = new ArrayList<NNLayer>();
@@ -29,76 +29,55 @@ public class DigitRecognition {
         list.add(layer3);
         //list.add(layer4);
         
-        NNet net = new NNet(list, 0.1);
+        NNet net = new NNet(list, 0.01);
         
-//	   double[][] inputs = new double[][]{
-//	            {0, 0, 1, 1},
-//	            {1, 1, 1, 1},
-//	            {1, 0, 1, 0},
-//	            {0, 1, 1, 1},
-//	            {0, 0, 0, 0},
-//	            {1, 0, 0, 1},
-//
-//	    };
-//	    double[][] outputs = new double[][]{
-//            {0,1},
-//            {1,1},
-//            {1,0},
-//            {0,1},
-//            {0,0},
-//            {1,1},
-//    };
-	    FileUtil fu = new FileUtil("Input.txt", true);
+	    FileUtil fu = new FileUtil("Input_2k.txt", true);
 	    double[][] inputFromFile = fu.getInput();
 	    double[][] outputFromFile = fu.getOutput();
 	    
 	    System.out.println("Training the neural net");
 	    long start = System.currentTimeMillis();
-	    net.backpropagation(inputFromFile, outputFromFile, 200);
+	    net.backpropagation(inputFromFile, outputFromFile, 2000);
 	    //check the Precision for output from training 
 	    //double output[][] = net.getOutput();
-	    //predictionCheckFromBackProp(inputFromFile, outputFromFile, net);
+	    predictionCheckFromBackProp(inputFromFile, outputFromFile, net);
 	    long finish = System.currentTimeMillis();
 	    long timeElapsed = finish - start;
 	    System.out.println("Finished training in (sec) "+ timeElapsed/1000);
 	    
-	    // calculate the predictions on unknown data
-        // 1, 0, 0
-        //predict(new double[][]{{0, 1, 0, 1}}, net);
-
-        // 0, 1, 0
-        //predict(new double[][]{{1, 1, 1, 0}}, net);
-	    
-	    FileUtil ftu = new FileUtil("Test_Input.txt", false);
+	    FileUtil ftu = new FileUtil("Test_Input_500.txt", false);
 	    double[][] testInputFromFile = ftu.getInput();
 	    double[][] testOutputFromFile = ftu.getOutput();
 	    
 	    predict(testInputFromFile, testOutputFromFile, net);
+	    long end_pgm = System.currentTimeMillis();
+	    System.out.println("Finished training in (sec) "+ (end_pgm-start_pgm)/1000);
 	    
     }
 	
 	public static void predictionCheckFromBackProp(double[][] testInput, double[][] ExpectedOutput, NNet net) {
         double output[][] = net.getOutput();
         
-        double predictedOutput[][] = predictedOutput(output);
+        double predictedOutput[][] = convertBinaryRepresentationToInt(output);
+        double actualOutput[][] = convertBinaryRepresentationToInt(ExpectedOutput);
         // Precision
         double precisionCount = 0;
         for(int i = 0;i< predictedOutput.length;i++)
         {  	
-        		if(predictedOutput[i][0] == ExpectedOutput[i][0] )
+        		if(predictedOutput[i][0] == actualOutput[i][0] )
         		{
         			precisionCount++;
         		}
         }
         double precision = precisionCount/predictedOutput.length;
-        System.out.println(precision);           
+        System.out.println("Precision from training data set " + precision);           
     }
 	
     public static void predict(double[][] testInput, double[][] ExpectedOutput, NNet net) {
         net.forwardpropagation(testInput);
         double output[][] = net.getOutput();
         
-        double predictedOutput[][] = predictedOutput(output);
+        double predictedOutput[][] = convertBinaryRepresentationToInt(output);
         // Precision
         double precisionCount = 0;
         for(int i = 0;i< predictedOutput.length;i++)
@@ -109,10 +88,10 @@ public class DigitRecognition {
         		}
         }
         double precision = precisionCount/predictedOutput.length;
-        System.out.println(precision);           
+        System.out.println("Precision from test data set " + precision);          
     }
     
-    public static double[][] predictedOutput( double output[][])
+    public static double[][] convertBinaryRepresentationToInt( double output[][])
 	{	
 		
 		double finalOutput[][] = new double[output.length][1];
