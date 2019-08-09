529db3ab0af73e8976aaaaab1e851f42c58614f2
gemithomas <42350582+gemithomas@users.noreply.github.com>
2019-08-09 15:57:53 -0400

    Add files via upload

---
+++ NeuralNetwork/src/test/java/NeuralAnalytics/NNUtilTest.java
@@ -0,0 +1,149 @@
+package NeuralAnalytics;
+
+import NeuralProjects.FileUtil;
+import org.junit.Test;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class NNUtilTest
+{	
+
+    @Test
+    public void matrixMultiplication()
+    {
+        double weightArr[][];
+
+        try
+        {
+            FileUtil fu = new FileUtil("Input.txt", true);
+
+            double[][] inputFromFile = fu.getInput();
+
+            weightArr = new double[784][20];
+
+            double output[][] = new double[inputFromFile.length][weightArr[0].length];
+
+            output = NNUtil.matrixMultiplication(inputFromFile, weightArr);
+
+            assertNotNull(output);
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void SigmoidFunctionOnMatrix()
+    {
+        double weightArr[][];
+
+        try
+        {
+            FileUtil fu = new FileUtil("Input.txt", true);
+
+            double[][] inputFromFile = fu.getInput();
+
+            weightArr = new double[784][20];
+
+            double output[][] = new double[inputFromFile.length][weightArr[0].length];
+
+            output = NNUtil.matrixMultiplication(inputFromFile, weightArr);
+
+            double[][] ans = NNUtil.applySigmoidFunc(output);
+
+            assertNotNull(ans);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void SigmoidDerivativeFunctionOnMatrix()
+    {
+        double weightArr[][];
+
+        try
+        {
+            FileUtil fu = new FileUtil("Input.txt", true);
+
+            double[][] inputFromFile = fu.getInput();
+
+            weightArr = new double[784][20];
+
+            double output[][] = new double[inputFromFile.length][weightArr[0].length];
+
+            output = NNUtil.matrixMultiplication(inputFromFile, weightArr);
+
+            double[][] matrix = NNUtil.applySigmoidFunc(output);
+
+            double[][] ans = NNUtil.applySigmoidDerivativeFunc(matrix);
+
+            assertNotNull(ans);
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+	@Test
+    public void NeuralNetworkCheck()
+    {
+    	NNLayer layer1 = new NNLayer(5, 4);
+        NNLayer layer2 = new NNLayer(1, 5);
+        ArrayList<NNLayer> list = new ArrayList<NNLayer>();
+        list.add(layer1);
+        list.add(layer2);
+        
+        NNet net = new NNet(list, 0.1);
+        
+        double[][] input = new double[][]{
+            {0, 0, 1, 1},
+            {1, 1, 1, 1},
+            {1, 0, 1, 0},
+            {0, 1, 1, 1},
+            {0, 0, 0, 0},
+            {1, 0, 0, 1},
+
+    };
+
+    double[][] output = new double[][]{
+            {0},
+            {1},
+            {1},
+            {0},
+            {0},
+            {1},
+    };
+    net.train(input, output, 10000);
+    
+    
+    double[][] testInput = new double[][]{{1, 1, 1, 0}};
+    double expectedOutput = 1;
+    net.forwardpropagation(testInput);
+    double testOuptput = net.getOutput()[0][0];
+    
+    double delta = 0.2;
+	assertEquals(expectedOutput, testOuptput, delta);
+    }
+	
+	@Test
+	public void sigmoidCalibration()
+	{
+		double y = 0.23;
+		
+		double actualOutput = 1 / (1 + Math.exp(-y));
+		double expectedOutput = NNUtil.sigmoidFunction(y);
+		
+		double delta = 0.2;
+		assertEquals(expectedOutput, actualOutput, delta);
+	}  
+
+}
